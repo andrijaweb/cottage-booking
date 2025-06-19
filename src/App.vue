ba7f028f039a1616dd83c7d96ea689ad8111e6fa
andrijaweb <andrijawebd@gmail.com>
2025-06-19 23:47:30 +0200

    add: added footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router'
 import HeaderSection from '@/components/organisms/HeaderSection.vue'
+import FooterSection from '@/components/organisms/FooterSection.vue'
 </script>
 
 <template>
   <HeaderSection />
   <RouterView />
+  <FooterSection />
 </template>
