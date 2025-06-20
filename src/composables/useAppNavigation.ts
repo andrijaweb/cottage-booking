2d99f168cc8e2e4682e0cc4afa86a95418f4055b
andrijaweb <andrijawebd@gmail.com>
2025-06-20 09:56:57 +0200

    add: added vue toastification, added loading spinner component

--- src/composables/useAppNavigation.ts
+++ src/composables/useAppNavigation.ts
@@ -1,9 +1,11 @@
 import { useRouter } from 'vue-router'
 import { useRoute } from 'vue-router'
+import { useToast } from 'vue-toastification'
 
 export default function useAppNavigation() {
   const router = useRouter()
   const route = useRoute()
+  const toast = useToast()
 
-  return { router, route }
+  return { router, route, toast }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import './assets/main.css'
+import 'vue-toastification/dist/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Toast from 'vue-toastification'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(Toast)
 app.use(createPinia())
 app.use(router)
 
