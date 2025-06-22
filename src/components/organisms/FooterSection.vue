ebae0e02b24304759246dd40bf8f147c64e46410
andrijaweb <andrijawebd@gmail.com>
2025-06-22 16:05:50 +0200

    remove: removed footer

--- src/components/organisms/FooterSection.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <footer>
-    <div class="bg-neutral-950 text-white py-4">
-      <div class="container mx-auto text-center">
-        &copy; 2023 Cottage Booking. All rights reserved.
-      </div>
-    </div>
-  </footer>
-</template>
