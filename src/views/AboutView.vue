84bfd02159a4bb6748c786187fa71671489c9544
andrijaweb <andrijawebd@gmail.com>
2025-06-25 23:59:20 +0200

    feat (about page): added about page view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1 class="text-3xl font-bold">This is an about page</h1>
+  <div class="min-h-screen flex items-center" :style="backgroundStyle">
+    <div class="container mx-auto px-4 pb-10 text-white">
+      <h1 class="text-5xl sm:text-7xl md:text-9xl font-playfair">Welcome to CottageBooker</h1>
+      <p class="my-5 max-w-2xl text-xl">
+        Your trusted platform for discovering and booking charming cottages in beautiful, serene
+        locations. At CottageBooking, we believe in helping people reconnect with nature.
+      </p>
+      <Button class="bg-yellow-500" size="lg">Explore Cottages</Button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import bgImage from '@/assets/imgs/about-bg.jpg'
+import { Button } from '@/components/ui/button'
+
+const backgroundStyle = computed(() => ({
+  background: `linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('${bgImage}')`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+}))
+</script>
